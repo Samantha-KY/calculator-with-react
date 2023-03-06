ffe88ed00fc2f24a82c8b0f7672d6acf58c8183e
Samiskina <mureraksamantha@gmail.com>
2023-03-06 17:35:19 +0200

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,97 +1,24 @@
-import { useState, useEffect } from "react";
+import useCalculator from "./hooks/useCalculator";
 import { NumericFormat } from "react-number-format";
+import DATA from "./constantData";
+import Buttons from "./components/Button";
 
 function App() {
-  const [preState, setPreState] = useState("");
-  const [curState, setCurState] = useState("");
-  const [input, setInput] = useState("0");
-  const [operator, setOperator] = useState(null);
-  const [total, setTotal] = useState(false);
-
-  const inputNum = (e) => {
-    if (curState.includes(".") && e.target.innerText === ".") return;
-
-    if (total) {
-      setPreState("");
-    }
-
-    curState
-      ? setCurState((pre) => pre + e.target.innerText)
-      : setCurState(e.target.innerText);
-    setTotal(false);
-  };
-
-  useEffect(() => {
-    console.log(curState)
-    setInput(curState);
-  }, [curState]);
-
-  useEffect(() => {
-    setInput("0");
-  }, []);
-  const operatorType = (e) => {
-    setTotal(false);
-    setOperator(e.target.innerText);
-    if (curState === "") return;
-    if (preState !== "") {
-      equals();
-    } else {
-      setPreState(curState);
-      setCurState("");
-    }
-  };
-
-  const equals = (e) => {
-    if (e?.target.innerText === "=") {
-      setTotal(true);
-    }
-    let cal;
-    switch (operator) {
-      case "/":
-        cal = String(parseFloat(preState) / parseFloat(curState));
-        break;
-
-      case "+":
-        cal = String(parseFloat(preState) + parseFloat(curState));
-        break;
-      case "X":
-        cal = String(parseFloat(preState) * parseFloat(curState));
-        break;
-      case "-":
-        cal = String(parseFloat(preState) - parseFloat(curState));
-        break;
-      default:
-        return;
-    }
-    setInput("");
-    setPreState(cal);
-    setCurState("");
-  };
-
-  const minusPlus = () => {
-    if (curState.charAt(0) === "-") {
-      setCurState(curState.substring(1));
-    } else {
-      setCurState("-" + curState);
-    }
-  };
-
-  const percent = () => {
-    preState
-      ? setCurState(String((parseFloat(curState) / 100) * preState))
-      : setCurState(String(parseFloat(curState) / 100));
-  };
-
-  const reset = () => {
-    setPreState("");
-    setCurState("");
-    setInput("0");
-  };
+  const {
+    input,
+    inputNum,
+    reset,
+    minusPlus,
+    equals,
+    operatorType,
+    percent,
+    preState,
+  } = useCalculator();
 
   return (
-    <div className='container'>
-      <div className='wrapper'>
-        <div className='calculation'>
+    <div className="container">
+      <div className="wrapper">
+        <div className="calculation">
           {input !== "" || input === "0" ? (
             <NumericFormat
               value={input}
@@ -106,67 +33,24 @@ function App() {
             />
           )}
         </div>
-        <div className='btn' onClick={reset}>
-          AC
-        </div>
-        <div className='btn' onClick={percent}>
-          %
-        </div>
-        <div className='btn' onClick={minusPlus}>
-          +/-
-        </div>
-        <div className='btn orange' onClick={operatorType}>
-          /
-        </div>
-        <div className='btn' onClick={inputNum}>
-          7
-        </div>
-        <div className='btn' onClick={inputNum}>
-          8
-        </div>
-        <div className='btn' onClick={inputNum}>
-          9
-        </div>
-        <div className='btn orange' onClick={operatorType}>
-          X
-        </div>
-        <div className='btn' onClick={inputNum}>
-          4
-        </div>
-        <div className='btn' onClick={inputNum}>
-          5
-        </div>
-        <div className='btn' onClick={inputNum}>
-          6
-        </div>
-        <div className='btn orange' onClick={operatorType}>
-          +
-        </div>
-        <div className='btn' onClick={inputNum}>
-          1
-        </div>
-        <div className='btn' onClick={inputNum}>
-          2
-        </div>
-        <div className='btn' onClick={inputNum}>
-          3
-        </div>
-        <div className='btn orange' onClick={operatorType}>
-          -
-        </div>
-        <div className='btn zero' onClick={inputNum}>
-          0
-        </div>
-        <div className='btn' onClick={inputNum}>
-          .
-        </div>
-        <div className='btn orange' onClick={equals}>
-          =
-        </div>
+        {DATA.map((_data, index) => {
+          return (
+            <Buttons
+              operatorType={operatorType}
+              minusPlus={minusPlus}
+              equals={equals}
+              percent={percent}
+              reset={reset}
+              inputNum={inputNum}
+              key={index}
+            >
+              {_data}
+            </Buttons>
+          );
+        })}
       </div>
     </div>
   );
 }
 
 export default App;
-
